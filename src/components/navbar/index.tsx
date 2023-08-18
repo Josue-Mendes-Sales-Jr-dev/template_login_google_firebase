291af30f88497d4054e709b66074e8522c875cef
Josué Mendes <109279870+Josue-Mendes-Sales-Jr-dev@users.noreply.github.com>
2023-08-18 11:49:09 -0300

    add styles

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import "tailwindcss/tailwind.css"
 interface NavbarProps{
     children?:React.ReactNode
@@ -8,9 +9,15 @@ interface NavbarProps{
 export const Navbar =(props:NavbarProps)=>{
     return(
         <>
-        <div className={`${props.className} bg-gray-500 flex flex-col w-1/12 h-[100vh] items-center justify-center`}
+        <div className={`${props.className} bg-gray-500 flex flex-col w-1/12 h-[100vh] items-center gap-5`}
         >
-              
+             <nav className="w-[100%]">
+                <ul className=" flex flex-col items-center justify-center gap-2 pt-10 w-[100%] border-box">
+                    <li className="hover:bg-green-600 rounded-tl-3xl rounded-bl-3xl h-[3rem] w-[100%]  flex items-center justify-center "><Link className="px-[1rem]" href="/">home</Link></li>
+                    <li className="hover:bg-green-600 rounded-tl-3xl rounded-bl-3xl h-[3rem] w-[100%]  flex items-center justify-center "><Link href="/">vídeos</Link></li>
+                    <li className="hover:bg-green-600 rounded-tl-3xl rounded-bl-3xl h-[3rem] w-[100%]  flex items-center justify-center "><Link href="/">formulário</Link></li>
+                </ul>
+            </nav> 
         </div>
         </>
     )
